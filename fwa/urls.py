b27921d2cd3df243f7d23646cd824c6ac5338648
Ted Lorts <tplorts@gmail.com>
2014-10-24 17:56:32 -0400

    (forgot to remove references to old model)

--- fwa/urls.py
+++ fwa/urls.py
@@ -2,17 +2,10 @@ from django.conf.urls import patterns, url, include
 
 from rest_framework import routers
 
-from fwa.views import (Wiew, ArtistViewSet, VenueViewSet, EventViewSet,
-                       ProductViewSet)
+from fwa.views import Wiew
 
-
-# pylint: disable=invalid-name
 
 router = routers.DefaultRouter()
-router.register(r'artists', ArtistViewSet)
-router.register(r'venues', VenueViewSet)
-router.register(r'events', EventViewSet)
-router.register(r'products', ProductViewSet)
 
 
 urlpatterns = patterns(
